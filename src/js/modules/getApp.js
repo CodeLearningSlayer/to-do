a1ffc6ed87d6b92aafdccf3e62c72aafc0f590ab
CodeLearningSlayer <rubtsov.maks2015@yandex.ru>
2022-09-13 17:45:30 +0300

    Added functionality, filters

--- src/js/modules/getApp.js
+++ src/js/modules/getApp.js
@@ -2,10 +2,11 @@ import { getTasks } from "../services/getAndSend.js";
 
 export default function getApp(){
     class Task {
-        constructor(text, parentSelector) {
+        constructor(text, type, parentSelector) {
             this.text = text;
             this.isCompleted = false;
             this.parent = parentSelector;
+            this.type = type;
         }
         render(){
             const task = document.createElement("li");
@@ -42,28 +43,32 @@ export default function getApp(){
             
            
             //повесить на input?
-            task.addEventListener("click", (e)=>{
-                const change = new Event("changeState");
-                if (e.target.className === task.className){
-                    console.log("создаю обработчики на изменение");
-                    if(!task.querySelector(".task__checkbox").checked){
-                        task.querySelector(".task__checkbox").checked = true;
-                        this.isCompleted = true;
-                        // this.dispatchEvent(change);
-                    }
-                    else{
-                        task.querySelector(".task__checkbox").checked = false;
-                        this.isCompleted = false;
-                        // this.dispatchEvent(change);
-                    }
-                }
+            task.addEventListener("click", (e) => {
+                console.log(e.target);
                 if (e.target.className === removeButton.className){
                     // task.remove(); // здесь как-то передать this в removeTask
                     const remove = new Event("delete");
                     task.dispatchEvent(remove); //запускать на том же элементе, что и отлавливать
                 }
-            });
+                else if(e.target.className === task.className){
+                    // e.stopImmediatePropagation();
+                    if(!task.querySelector(`.${checkbox.className}`).checked){
+                        task.querySelector(`.${checkbox.className}`).checked = true;
+                    }
+                    else{
+                        task.querySelector(`.${checkbox.className}`).checked = false;
+                    }
+                    task.querySelector(`.${checkbox.className}`).onchange();
+                }
+                
+            }, true);
 
+  
+            task.querySelector(`.${checkbox.className}`).onchange = () =>{
+                const remove = new Event("changeState");
+                this.isCompleted = !this.isCompleted;
+                this.elem.dispatchEvent(remove);
+            };
 
             if (this.isCompleted){
                 task.querySelector(".task__checkbox").checked = true;
@@ -71,6 +76,10 @@ export default function getApp(){
 
             
             this.parent.append(task);
+        }
+
+        replace(newParentSelector){
+            this.parent = newParentSelector;
         }
         
     }
@@ -88,6 +97,8 @@ export default function getApp(){
             this.completed = [];
             this.toDo = [];
             this.collection = collection;
+            this.visibleToDoList = [];
+            this.visibleCompletedList = [];
         }
 
         createAddInput(parentSelector){
@@ -111,7 +122,7 @@ export default function getApp(){
 
             addInputForm.addEventListener("keydown", (e)=>{
                 if (e.target.className === input.className && e.code === "Enter"){
-                    let newTask = new Task(e.target.value, this.toDoList);
+                    let newTask = new Task(e.target.value, document.querySelector(".nav__item--active").textContent.toLowerCase(), this.toDoList);
 
                     this.toDo.push(newTask);
                     this.renderList(this.toDo);
@@ -147,26 +158,107 @@ export default function getApp(){
             document.querySelector(parentSelector).append(this.completedList);
         }
 
+        renderWindow(firstList, secondList){
+            
+            this.renderList(firstList);
+            this.renderList(secondList);
+
+            this.createListeners(firstList);
+            this.createListeners(secondList);
+
+            this.refreshTaskCount(".tasks__title", "toDo");
+            this.refreshTaskCount(".completed__title", "completed");
+        }
+
         refreshTaskCount(selector, type){
             if(type === "toDo"){
-                document.querySelector(selector).textContent = `Tasks - ${this.toDo.length}`;
+                document.querySelector(selector).textContent = `Tasks - ${document.querySelector(selector).nextSibling.children.length}`;
             }
             else{
-                document.querySelector(selector).textContent = `Completed - ${this.completed.length}`;
+                document.querySelector(selector).textContent = `Completed - ${document.querySelector(selector).nextSibling.children.length}`;
             }
         }
 
-        addCollections(){
-            const dailyTasks = document.createElement("div");
-            const todayTasks = document.createElement("div");
-            const planTasks = document.createElement("div");
+        addCollections(parentSelector){
 
-            dailyTasks.innerText = "Daily";
-            todayTasks.innerText = "Today";
-            planTasks.innerText = "Plans";
+            const collections = document.createElement("div");
+            const title = document.createElement("h6");
+            const itemsList = document.createElement("ul");
 
-            dailyTasks.addEventListener("click", ()=>{
+            collections.className = "collections";
+            title.className = "collections__title";
+            itemsList.className = "collections__nav";
+            
+            title.textContent = "Collections";
+            
+            const collectionArr=[];
 
+            for(let i=0; i<3; i++){
+                const collection = document.createElement("li");
+                collection.addEventListener("click", function(){
+                    collectionArr.forEach(item => item.classList.remove("nav__item--active"));
+                    this.classList.add("nav__item--active");
+                });
+                 collectionArr.push(collection);
+
+            }
+            
+            for(let i=0; i<collectionArr.length; i++){
+                collectionArr[i].classList.add("nav__item");
+                switch(i){
+                    case 0:
+                        collectionArr[i].classList.add("nav__item--daily");
+                        collectionArr[i].classList.add("nav__item--active");
+                        collectionArr[i].textContent = "Daily";
+                        break;
+                    case 1:
+                        collectionArr[i].classList.add("nav__item--today"); 
+                        collectionArr[i].textContent = "Today";
+                         break;
+                    case 2:
+                        collectionArr[i].classList.add("nav__item--plans"); 
+                        collectionArr[i].textContent = "Plans";
+                        break;
+                }
+            }
+            
+            collectionArr.forEach(tab => {
+                itemsList.append(tab);
+            });
+
+            collections.append(title);
+            collections.append(itemsList);
+            document.querySelector(parentSelector).append(collections);
+
+            collections.addEventListener("click", (e)=>{
+                if (e.target.classList.contains("nav__item--daily")){
+
+                    this.visibleToDoList = this.toDo.filter(task => task.type === "daily");
+                    this.visibleCompletedList = this.completed.filter(task => task.type === "daily");
+
+                    console.log(this.visibleToDoList);
+                    console.log(this.visibleCompletedList);
+
+                    this.renderWindow(this.visibleToDoList, this.visibleCompletedList);
+
+                }
+                if (e.target.classList.contains("nav__item--today")){
+                
+                    this.visibleToDoList = this.toDo.filter(task => task.type === "today");
+                    this.visibleCompletedList = this.completed.filter(task => task.type === "today");
+
+                    this.renderWindow(this.visibleToDoList, this.visibleCompletedList);
+
+
+                }
+                if (e.target.classList.contains("nav__item--plans")){
+                    
+                    this.visibleToDoList = this.toDo.filter(task => task.type === "plans");
+                    this.visibleCompletedList = this.completed.filter(task => task.type === "plans");
+
+                    this.renderWindow(this.visibleToDoList, this.visibleCompletedList);
+
+                }
             });
         }
 
@@ -174,10 +266,10 @@ export default function getApp(){
         createList(data, stateOfTasks){ //создание списка объектов задач
             data.forEach(task => {
                 if (stateOfTasks === "toDo"){
-                    this.toDo.push(new Task(task.text, this.toDoList));
+                    this.toDo.push(new Task(task.text, task.type, this.toDoList));
                 }
                 if (stateOfTasks === "completed"){
-                    let newTask = new Task(task.text, this.completedList);
+                    let newTask = new Task(task.text, task.type, this.completedList);
                     newTask.isCompleted = true;
                     this.completed.push(newTask);
                 }
@@ -190,22 +282,19 @@ export default function getApp(){
         }
 
         removeTask(task, tasksArr){ //работа с dom-деревом
-            if (task.isCompleted){
-                this.completed.splice(this.completed.indexOf(task), 1); //запуск рендера по новой
-                this.refreshTaskCount(".completed__title", "completed");
-            }
-            else{
-                this.toDo.splice(this.toDo.indexOf(task), 1);
-                this.refreshTaskCount(".tasks__title", "toDo");
-            }
-            console.log(tasksArr);
+
+            tasksArr.splice(tasksArr.indexOf(task), 1);
+
+            this.refreshTaskCount(".tasks__title", "toDo");
+            this.refreshTaskCount(".completed__title", "completed");
+
             this.renderList(tasksArr);
             this.createListeners(tasksArr);
 
         }
 
         renderList(tasksArr){ //рендерим список задач
-            if(tasksArr === this.toDo){
+            if(tasksArr === this.toDo || tasksArr === this.visibleToDoList){
                 this.toDoList.innerHTML = "";
             }
             else{
@@ -221,26 +310,39 @@ export default function getApp(){
                 task.elem.addEventListener("delete", () => {
                     this.removeTask(task, tasksArr);
                 });
+
                 task.elem.addEventListener("changeState", () => {
                     console.log(task);
                     if(task.isCompleted){
+                        console.log(task.text + " completed");
+                        task.replace(this.completedList);
                         this.completed.push(task);
-                        this.toDo.splice(this.toDo.indexOf(task));
+                        this.toDo.splice(this.toDo.indexOf(task), 1);
                     }
                     else{
+                        console.log(task.text + " is not completed");
                         this.toDo.push(task);
-                        this.completed.splice(this.completed.indexOf(task));
+                        task.replace(this.toDoList);
+                        this.completed.splice(this.completed.indexOf(task), 1);
                     }
-                    // this.renderList(this.toDo);
-                    // this.renderList(this.completed);
+
+                    this.renderList(this.toDo);
+                    this.createListeners(this.toDo);
+
+                    this.renderList(this.completed);
+                    this.createListeners(this.completed);
+
+                    this.refreshTaskCount(".tasks__title", "toDo");
+                    this.refreshTaskCount(".completed__title", "completed");
+                
                 });
 
-                });
+            });
         }
             
 
         async start(){
-            
+            this.addCollections(".left-side");
             this.createAddInput(".right-side");
             
             this.createToDoList(".right-side .tasks");
